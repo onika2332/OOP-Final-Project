1ec7c865c59e39b037a18aa045408042a7a1277d
mvlinh <linh.mv184132@sis.hust.edu.vn>
2021-06-04 22:49:26 +0700

    check vuong goc va lay ra chieu dai cua vecto

--- src/model/Vector.java
+++ src/model/Vector.java
@@ -15,14 +15,30 @@ public class Vector extends Vector3D_F32 {
 		super(p1,p2);
 	}
 	
-	public boolean checkParallel(Vector v) {
+	public boolean isParallel(Vector v) {
 		float ratio1 = this.getX() / v.getX();
 		float ratio2 = this.getY() / v.getY();
 		float ratio3 = this.getZ() / v.getZ();
 		
 		return (ratio1 == ratio2) && (ratio1 == ratio3);
 	}
-	
-	
+	public boolean isPerpendicular(Vector v) {
+		return 0 == ( this.getX()*v.getX() 
+					+ this.getY()*v.getY() 
+					+ this.getZ()*v.getZ());
+	}
+	public float vectoLeng() {
+		float leng = (float) Math.sqrt(this.getX()*this.getX() 
+									 + this.getY()*this.getY() 
+									 + this.getZ()*this.getZ());
+		return leng;
+	}
+	public  Vector Directional(Vector a, Vector b) {
+		float n1 = (a.getY()*b.getZ() - a.getZ()*b.getY());
+		float n2 = -(a.getX()*b.getZ() - a.getZ()*b.getX());
+		float n3 = (a.getX()*b.getY() - a.getY()*b.getX());
+		Vector n = new Vector(n1,n2,n3);
+		return n;
+	}
 }
 
