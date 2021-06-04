42f0668a2d8a0f9ccfa9438fd0c1214f0d6ccc41
mvlinh <linh.mv184132@sis.hust.edu.vn>
2021-06-04 22:48:46 +0700

    tao ra tuong va do dai ,rong ,cao cua hop chu nhat

--- src/model/Room.java
+++ src/model/Room.java
@@ -8,7 +8,12 @@ public class Room extends AbstractObject {
 	private float length;
 	private float width;
 	private float height;
-	List<Plane> walls;
+	List<Plane> walls = new ArrayList<Plane>();
+	public List<Plane> getWalls() {
+		return walls;
+	}
+
+
 	public float getLength() {
 		return length;
 	}
@@ -36,19 +41,51 @@ public class Room extends AbstractObject {
 	public Room() {
 		super();
 	}
-	
+	public Plane roomPlane(Vector v, Point p1) {
+		float A = v.getX();
+		float B = v.getY();
+		float C = v.getZ();
+		float D = (float)(v.getX()*p1.getX() 
+						+ v.getY()*p1.getY() 
+						+ v.getZ()*p1.getZ());
+		Plane p = new Plane(A,B,C,D);
+		return p;
+	}
 	public Room(Point p1, Point p2, Point p3, Point p4,
 				Point p5, Point p6, Point p7, Point p8) {
 		super(p1,p2,p3,p4,p5,p6,p7,p8);
-		for(float i = p1.getX(); i <= p7.getX(); i+= 0.01) {
-			for(float j = p1.getY(); i <= p7.getY(); j+= 0.01) {
-				for(float k = p1.getZ(); i <= p7.getZ(); k+= 0.01) {
-					Point p = new Point(i,j,k);
-					p.setState(State.Available);
-					// we have conflict with constructor of Object
-				}
-			}
-		}
+		Vector p12 = new Vector(p1,p2);	//  p1,5,8,4; p2,3,7,6 mat ben
+		Vector p41 = new Vector(p4,p1);	//  p1,2,6,5; p4,3,7,8	mat ben
+		Vector p15 = new Vector(p1,p5); //  p1,2,3,4; p5,6,7,8 mat day
+		
+		Plane p1234 = roomPlane(p15,p1);
+		Plane p5678 =  roomPlane(p15,p5);
+		
+		Plane p1584 = roomPlane(p12, p1);
+		Plane p2376 = roomPlane(p12, p2);
+		Plane p1265 = roomPlane(p41, p1);
+		Plane p4378 = roomPlane(p41, p4);
+		
+		walls.add(0,p1234);
+		walls.add(p5678);
+		walls.add(p1584);
+		walls.add(p2376);
+		walls.add(p1265);
+		walls.add(p4378);
+		
+		setHeight(p12.vectoLeng());
+		setWidth(p41.vectoLeng());
+		setLength(p12.vectoLeng());
+		
+//		for(float i = p1.getX(); i <= p7.getX(); i+= 0.01) {
+//			for(float j = p1.getY(); i <= p7.getY(); j+= 0.01) {
+//				for(float k = p1.getZ(); i <= p7.getZ(); k+= 0.01) {
+//					Point p = new Point(i,j,k);
+//					p.setState(State.Available);
+//					// we have conflict with constructor of Object
+//				}
+//			}
+//		}
 	}
 	
 }
