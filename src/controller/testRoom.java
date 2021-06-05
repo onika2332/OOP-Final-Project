b351c568e8b48c69bfe0cbe3c8395c498f6fcee9
namnam <namnam@coder.com>
2021-06-06 01:04:30 +0700

    Chane name of class AbstractObject from AbstractObject to Box

--- src/controller/testRoom.java
+++ src/controller/testRoom.java
@@ -16,7 +16,6 @@ public class testRoom{
 		Point p8 = new Point(0,3,2);
 		Room r  = new Room(p1,p2,p3,p4,p5,p6,p7,p8);
 		if(r.isBox(p1,p2,p3,p4,p5,p6,p7,p8)) {
-				//chuong trinh chinh:
 			System.out.println(r.getWalls());
 			System.out.println(r.getHeight());
 			System.out.println(r.getWidth());
